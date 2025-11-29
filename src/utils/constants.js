b7087e1640c5351cbb08db4075b4a2ad136e2d85
Khoa Dam <damngockhoa0703@gmail.com>
2025-11-29 08:29:22 +0700

    Update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,15 @@
+import { ENV } from '~/config/environment'
 
+// Danh sách các domain được phép gọi API (CORS whitelist)
+// Chỉ các origin trong mảng này mới được nhận header `Access-Control-Allow-Origin`
+export const WHITELIST_DOMAINS = [
+    // Domain FE ở môi trường dev (nếu có)
+    ENV.WEBSITE_DOMAIN_DEVELOPMENT,
+
+    // Domain FE ở môi trường production (nếu có cấu hình trong .env)
+    ENV.WEBSITE_DOMAIN_PRODUCTION,
+
+    // Domain Vercel hiện tại của bạn
+    'https://frontend-theta-ten-83.vercel.app'
+].filter(Boolean)
 
