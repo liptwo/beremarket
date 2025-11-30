f84269cb1e53276baa6ea1a4dec87fd1dcf7ea22
Đào Huỳnh <142609541+liptwo@users.noreply.github.com>
2025-12-01 05:12:54 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,6 @@ import { ENV } from '~/config/environment'
 export const WHITELIST_DOMAINS = [
   'http://localhost:5017',
   'http://127.0.0.1:3000',
+  'https://frontend-theta-ten-83.vercel.app',
   ENV.WEBSITE_DOMAIN_PRODUCTION
 ]
